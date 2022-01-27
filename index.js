83d17344287687e048b132cf3b3181b11bcc040a
lloydonix <leolloydcatalan28@gmail.com>
2022-01-27 18:25:41 +0800

    test_to_main3

---
+++ index.js
@@ -0,0 +1,36 @@
+window.onresize = function(event) {
+    document.location.reload(true);
+  }
+
+  window.onresize = function(){ location.reload(); }
+
+  var href = window.location.href.split("/")
+  var html_location = href[href.length-1]
+  
+  if (window.innerWidth >= 960 && html_location !== "index.html") {
+      window.location = "index.html";
+  }
+  
+  if (window.innerWidth < 500 && html_location !== "index2.html") {
+      window.location = "index2.html";
+  }
+
+  /* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
